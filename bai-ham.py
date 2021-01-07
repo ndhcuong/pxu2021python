a43b2d5fd4bac5b2cdf214d09de3a556d1a7375e
ndhcuong <ndhcuong@gmail.com>
2021-01-07 07:49:39 +0700

    ndhcuong

--- bai-ham.py
+++ bai-ham.py
@@ -10,3 +10,12 @@ y = 7
 
 print(tong_tich_hieu_thuong(x,y))
 
+ket_qua = tong_tich_hieu_thuong(x, y)
+print(ket_qua)
+
+tong, tich, hieu, thuong = tong_tich_hieu_thuong(x, y)
+print(tong, tich, hieu, thuong)
+
+_, tich, hieu, thuong = tong_tich_hieu_thuong(x, y)
+print(tich)
+
