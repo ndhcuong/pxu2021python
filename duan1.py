713f0ec3bbc525533b7b4a2ad6990455111d030b
ndhcuong <ndhcuong@gmail.com>
2021-01-07 08:16:39 +0700

    ndhcuong

---
+++ duan1.py
@@ -0,0 +1,9 @@
+import thuvien as tv
+
+
+a = 5
+b = 7
+
+
+kq = tv.tinh_tich(a, b)
+print(kq)
---
+++ thuvien.py
@@ -0,0 +1,8 @@
+def tinh_tong(x,y):
+    return x + y
+
+def tinh_hieu(x, y):
+    return x - y
+
+def tinh_tich(x, y):
+    return x*y
